5f5b0392ec164a08ba726c28a59d8dd966693859
kravenhunter <kravenhunter2012@yandex.ru>
2023-12-17 15:44:58 +0300

    profile

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,42 +1,52 @@
 <template>
-    <div>
-        <pre>
+  <div>
+    <pre v-if="user && !errors">
             {{ user }}
-        </pre>
+        </pre
+    >
+    <div class="" v-else>
+      <pre>
+            {{ errors?.message }}
+            {{ errors?.statusCode }}
+            {{ errors?.statusMessage }}
+        </pre
+      >
     </div>
+  </div>
 </template>
 
-<script setup>
-    const user = ref(null);
-    const cookie = useCookie("jwt-token");
-    const errors = ref(null);
-    const sub = async () => {
-        try {
-            const { data, error } = await useFetch("http://localhost:5281/api/Auth/user/check", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json; charset=utf-8",
-                Bearer: cookie.value
-            },
-        });
-            if (error.value) {
-                throw error.value;
-            }
-            cookie.value = data.value.data.token;
-            console.log(data.value)
-            if(data.value){
-                user.value = data.value
-            }
-            } catch (error) {
-                console.log(error);
-                errors.value = error;
-            }  
-        };
-        onMounted(async()=>{
-            await sub()
-        })
+<script setup lang="ts">
+import type { H3Error } from "h3";
+const user = ref<Object | null>(null);
+const cookie = useCookie("jwt-token");
+const errors = ref<H3Error | null>(null);
+try {
+  const { data, error } = await useFetch("http://localhost:5281/api/Auth/user/check", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      Authorization: cookie.value ? `Bearer ${cookie.value}` : "",
+      //   Bearer: cookie.value ? cookie.value : "",
+    },
+  });
+  if (error.value) {
+    throw error.value;
+  }
+
+  console.log(data.value);
+  if (data.value) {
+    user.value = data.value;
+  }
+} catch (error) {
+  console.log("Error", error);
+  errors.value = error as H3Error;
+}
+// const sub = async () => {
+
+// };
+// onMounted(async () => {
+//   await sub();
+// });
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
